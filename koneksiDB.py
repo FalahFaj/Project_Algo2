981c303d376a15ee1b0ae0e4faf060315edccfd5
Muhammad_Falah <muhammadfalahfajrul0@gmail.com>
2025-05-18 19:32:02 +0700

    ngubah database

--- koneksiDB.py
+++ koneksiDB.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def connect():
     try:
-        conn = psycopg2.connect("dbname='Cantiknya Mahar' user='postgres' host='125.161.191.47' password= 'ns4dpn'port='5432'")
+        conn = psycopg2.connect("dbname='Tubes Algo 2' user='postgres' host='125.161.191.47' password= 'ns4dpn'port='5432'")
         print("Koneksi Berhasil")
     except Exception as e:
         print("Koneksi Gagal")
